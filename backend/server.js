4a104af494a1ef2e88b262608b29d14b411c3e46
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-06-09 12:55:15 +0800

    Implement server to serve react client

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 // TODO: Require route files
 const apiRoutes = require('./routes/api-routes');
@@ -25,6 +26,14 @@ if (process.env.NODE_ENV === 'development') {
 
 // TODO: Mount routers
 app.use('/api', apiRoutes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
